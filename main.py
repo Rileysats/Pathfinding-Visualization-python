a4015eeef23072d779c882de14bc939ee72060ea
Rileysats <50492595+Rileysats@users.noreply.github.com>
2021-05-17 22:34:36 +0930

    tkinter popup finished

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter.constants import END
 import pygame
 
 black = (0, 0, 0)
@@ -23,34 +24,51 @@ window_size = (31*margin+30*height, 31*margin+30*width)
 
 algos = ["Dijkstra's algorithm", "A* algorithm", "Sample algorithm"]
 btn_list = []
-def algoSelect():
-  print('s')
+def algoSelect(num) -> None:
+  for i in range(len(btn_list)):
+    if i != num:
+      btn_list[i].configure(fg="red")
+    else:
+      btn_list[num].configure(fg="green")
  
-def onsubmit():
-  print('a')
+def onsubmit() -> None:
+  global start
+  global end
+  start = grid[int(startBoxx)][int(startBoxy)]
+  end = grid[int(endBoxx)][int(endBoxy)]
+  popup.quit()
+  popup.destroy()
+  
 popup = tk.Tk()
-start = tk.Label(popup, text='Start(x,y): ')
-startBox = tk.Entry(popup)
-end = tk.Label(popup, text='End(x,y): ')
-endBox = tk.Entry(popup)
+text = tk.Label(popup, text ="Enter co-ords for start and end point and path finding algorithm.")
+start = tk.Label(popup, text="Start(x,y): ")
+startBoxx = tk.Entry(popup)
+startBoxy = tk.Entry(popup)
+end = tk.Label(popup, text="End(x,y): ")
+endBoxx = tk.Entry(popup)
+endBoxy = tk.Entry(popup)
 var = tk.IntVar()
 
 # showPath = tk.Checkbutton(popup, text='Show Steps :', onvalue=1, offvalue=0, variable=var)
 # algo =
 
-submit = tk.Button(popup, text='Submit', command=onsubmit)
+submit = tk.Button(popup,pady=8,padx=12,fg='green',font=("Arial",20), text='Submit', command=onsubmit)
 
 # showPath.grid(columnspan=2, row=2)
-startBox.grid(row=0, column=1, pady=3)
-end.grid(row=0, pady=3)
+text.grid(row=0,columnspan=2)
+startBoxx.grid(row=1, column=2)
+startBoxy.grid(row=1, column=3)
+end.grid(row=2, pady=3)
 start.grid(row=1, pady=3)
-endBox.grid(row=1, column=1, pady=3)
+endBoxx.grid(row=2, column=1)
+endBoxy.grid(row=2, column=2)
 
 for i,v in enumerate(algos):
-  b = tk.Button(popup,command= algoSelect,text=v)
-  b.grid(row=3,column=i)
+  b = tk.Button(popup,command=lambda i=i: algoSelect(i),text=v,fg="red")
+  b.grid(row=4,column=i)
+  btn_list.append(b)
 
-submit.grid(columnspan=2, row=4)
+submit.grid(column=1,row=5)
 
 popup.update()
 tk.mainloop()
