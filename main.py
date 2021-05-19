7ccc4a2c87bbaf49b52a46c3cf39c55e84563fce
Rileysats <50492595+Rileysats@users.noreply.github.com>
2021-05-19 10:20:16 +0930

    Project visuals finished

--- main.py
+++ main.py
@@ -11,112 +11,164 @@ RED = (255, 0, 0)
 GREY = (130,130,130)
 WIDTH_OF_SQUARE = 15
 HEIGHT_OF_SQUARE = 15
-MARGIN = 2
 ROW = 30
 COLUMN = 30
 SQAURES = 30
-WINDOW_SIZE = ((SQAURES+1)*MARGIN+SQAURES*WIDTH_OF_SQUARE, (SQAURES+1)*MARGIN+SQAURES*HEIGHT_OF_SQUARE+30)
+WINDOW_SIZE = (SQAURES*WIDTH_OF_SQUARE, SQAURES*HEIGHT_OF_SQUARE+30)
 GRID = []
-# algos = ["Dijkstra's algorithm", "A* algorithm", "Sample algorithm"]
-# BTN_LIST = []
 
 class Path:
 
   def __init__(self) -> None:
-      pass
+      self.start = None
+      self.end = None
+      self.algo = None
   
-  def set_start(self,start):
+  def set_start(self,start) -> None:
     self.start = start
   
-  def set_end(self,end):
+  def set_end(self,end) -> None:
     self.end = end
+
+  def set_algo(self,algo) -> None:
+    self.algo = algo
 
 
 
 # PYGAME GRID INTIALISE
-def make_grid():
+def make_grid() -> None:
   for row in range(ROW):
       GRID.append([])
       for column in range(COLUMN):
           GRID[row].append(0) 
 
 # TKINTER POPUP
-
 def set_algo(num,path,btn_list,algos) -> None:
-  global algo 
-  algo = algos[num]
+  path.set_algo(algos[num])
   for i in range(len(btn_list)):
     if i != num:
       btn_list[i].configure(fg="red")
     else:
       btn_list[num].configure(fg="green")
 
+def on_submit(root,path) -> None:
+  if path.algo:
+    root.destroy()
+    root.quit()
+
 def algo_select(path):
   algos = ["Dijkstra's algorithm", "A* algorithm", "Sample algorithm"]
   btn_list = []
 
-  popup = tk.Tk()
-  popup.title('Pathfinding algorithm')
-  text = tk.Label(popup, text ="Choose path finding algorithm.")
+  root = tk.Tk()
+  root.title("Pathfinding algorithm")
+  text = tk.Label(root, text ="Choose path finding algorithm.")
+  root.eval('tk::PlaceWindow . center')
+  submit = tk.Button(root,pady=8,padx=12,fg='green',font=("Arial",20), text='Submit', command=lambda: on_submit(root,path))
 
-  submit = tk.Button(popup,pady=8,padx=12,fg='green',font=("Arial",20), text='Submit', command=popup.destroy)
   text.grid(row=0,columnspan=3)
-
   submit.grid(column=1,row=2)
 
   for i,v in enumerate(algos):
-    b = tk.Button(popup,command=lambda i=i: set_algo(i,path,btn_list,algos),text=v,fg="red")
+    b = tk.Button(root,command=lambda i=i: set_algo(i,path,btn_list,algos),text=v,fg="red")
     b.grid(row=1,column=i)
     btn_list.append(b)
 
-  popup.update()
-  tk.mainloop()
+  root.mainloop()
 
+
+def tell_user() -> None:
+  root = tk.Tk()
+  root.title("Pathfinding algorithm")
+  root.eval('tk::PlaceWindow . center')
+  tk.Label(root, text ="First click is start point, second click is destination").pack()
+  tk.Button(root,pady=6,padx=8,fg='green',font=("Arial",10), text='OK', command=root.destroy).pack()
+  root.mainloop()
+
+def draw_grid(screen) -> None:
+  for row in range(len(GRID)):
+    for column in range(len(GRID[0])):
+      color = WHITE
+      pygame.draw.rect(screen,color,
+                              [(WIDTH_OF_SQUARE) * column,
+                                ( HEIGHT_OF_SQUARE) * row +30, WIDTH_OF_SQUARE, HEIGHT_OF_SQUARE])
+
+def draw_lines(screen) -> None:
+  for row in range(len(GRID)):
+    pygame.draw.line(screen,BLACK,(WIDTH_OF_SQUARE*row,30),(WIDTH_OF_SQUARE*row,WINDOW_SIZE[1]),1)
+    for column in range(len(GRID[0])):
+      pygame.draw.line(screen,BLACK,(0,WIDTH_OF_SQUARE*column+30),(WINDOW_SIZE[0],WIDTH_OF_SQUARE*column+30),1)
+
+def update_grid(screen) -> None:
+  for row in range(len(GRID)):
+    for column in range(len(GRID[0])): 
+      color = None
+      if GRID[row][column] == 1: #BARRIER
+        color = GREY
+      if GRID[row][column] == 2: #START
+        color = RED
+      if GRID[row][column] == 3: #END
+        color = BLACK
+      if color:
+        pygame.draw.rect(screen,color,
+                              [(WIDTH_OF_SQUARE) * column,
+                                ( HEIGHT_OF_SQUARE) * row +30, WIDTH_OF_SQUARE, HEIGHT_OF_SQUARE])
 
 # PYGAME SETUP
-def main() -> None:
+def main(path) -> None:
   pygame.init()
   screen = pygame.display.set_mode(WINDOW_SIZE)
-  pygame.display.set_caption(algo)
+  pygame.display.set_caption(path.algo)
+  screen.fill(BLACK)
   done = False
   clock = pygame.time.Clock()
+  game_text = "start"
+  stop_drawing = False
+  started = False
+  draw_grid(screen)
+  draw_lines(screen)
   while not done:
     position = pygame.mouse.get_pos()
+    column = position[0] // WIDTH_OF_SQUARE
+    row = position[1] // HEIGHT_OF_SQUARE-2
+
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         done = True
-      elif pygame.mouse.get_pressed()[0] and position[1] > 33:
-        
-        column = position[0] // (WIDTH_OF_SQUARE + MARGIN)
-        row = position[1] // (HEIGHT_OF_SQUARE + MARGIN)-2
-        if row < ROW and column < COLUMN:
-          if (row == start[1] and column == start[0]) or (row == end[1] and column == end[0]):
-            continue
-          GRID[row][column] = 1
+      elif pygame.mouse.get_pressed()[0] and position[1] > 33 and row < ROW and column < COLUMN and stop_drawing == False:
+        if not path.start:
+          path.set_start((row,column))
+          GRID[row][column] = 2
+          continue
+        elif not path.end and (row,column) != path.start:
+          path.set_end((row,column))
+          GRID[row][column] = 3
+          continue
+        elif (row == path.start[0] and column == path.start[1]) or (row == path.end[0] and column == path.end[1]):
+          continue
+        GRID[row][column] = 1
 
-      elif event.type == pygame.MOUSEBUTTONDOWN and 0 < position[1] < 33 and (WINDOW_SIZE[0]//2)-35 < position[0] < (WINDOW_SIZE[0]//2)+35:
-        # find()
+      elif event.type == pygame.MOUSEBUTTONDOWN and 0 < position[1] < 33 and (WINDOW_SIZE[0]//2)-35 < position[0] < (WINDOW_SIZE[0]//2)+35 and game_text == "start":    
+        game_text = "stop"
+        started = True
+        stop_drawing = True
+      elif event.type == pygame.MOUSEBUTTONDOWN and 0 < position[1] < 33 and (WINDOW_SIZE[0]//2)-35 < position[0] < (WINDOW_SIZE[0]//2)+35 and game_text == "stop":
+        game_text = "start"
+        started = False
+      elif started:
         pass
-    screen.fill(BLACK)
-      
-    for row in range(len(GRID)):
-      for column in range(len(GRID[0])):
-        color = WHITE
-        if GRID[row][column] == 1: #BARRIER
-          color = GREY
-        if GRID[row][column] == 2: #START
-          color = RED
-        if GRID[row][column] == 3: #END
-          color = BLACK
-        pygame.draw.rect(screen,color,
-                              [(MARGIN + WIDTH_OF_SQUARE) * column + MARGIN,
-                                (MARGIN + HEIGHT_OF_SQUARE) * row + MARGIN+30, WIDTH_OF_SQUARE, HEIGHT_OF_SQUARE])
-    pygame.draw.rect(screen,WHITE,((WINDOW_SIZE[0]//2)-35,2,70,28))
-    font = pygame.font.SysFont('times new roman',35)
-    text = font.render("start",1,(0,0,0))
-    screen.blit(text,((WINDOW_SIZE[0]//2)-32,-4))       
-    clock.tick(60)
 
+
+
+      
+      update_grid(screen)
+      
+    pygame.draw.rect(screen,WHITE,((WINDOW_SIZE[0]//2)-35,1,70,28))
+    font = pygame.font.SysFont('times new roman',35)
+    text = font.render(game_text,1,(0,0,0))
+    screen.blit(text,((WINDOW_SIZE[0]//2)-32,-7))
+ 
+    clock.tick(60)
     pygame.display.flip()
 
   pygame.quit()
@@ -125,5 +177,6 @@ if __name__ == "__main__":
   path = Path()
   make_grid()
   algo_select(path)
-  main()
+  tell_user()
+  main(path)
 
